e13d869ff542a49d335ac67a6031bfd3960209e6
unknown <mohammad.nadom98@gmail.com>
2021-03-24 09:34:29 +0300

    initial commit

---
+++ src/Components/Cards/Cards.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Card from "../Card/Card.js"
+
+const Cards = () => {
+    return (
+            <div className="container">
+                <div className="row">
+                        <Card className="col-sm"/>
+                        <Card className="col-sm"/>
+                        <Card className="col-sm"/>
+                        <Card className="col-sm"/>
+                </div>
+            </div>
+
+    )
+}
+
+export default Cards;
