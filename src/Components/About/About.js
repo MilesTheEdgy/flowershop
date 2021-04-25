5074ac03b118454461243ee822a4af0489910401
Muhammet <mohammad.nadom98@gmail.com>
2021-04-25 14:47:27 +0300

    added fixes to the about page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -15,50 +15,41 @@ const About = () => {
                 <p className = "paragraph-2" >here is some testimoney from our clients</p>
 
 
-                <div className = "container man-testimony-div">
-                    <div className = "row">
-                        <div className = "col-sm-1">
-                           <img src = {Man} width = "400" height = "400" className = "man-img col" />
-                        </div>
-                        <div className = "col-sm-11">
-                            <p className = "man-p"> ''I was in dire need of a quick way to get my flowers
-                                on time for my wedding day, I would like to thank Flower
-                                Shop for their efforts on showing up in time.''</p>
-                            <p className = "man-title">
-                                -Melvin Jones
-                            </p>
-                        </div>
-                    </div>
-                </div>
+                <div className = " testimony-container">
 
-                <div className = "Woman-testimony-div">
-                    <img src = {Woman} width = "400" height = "400" className = "Woman-img" />
-                    <div>
-                        <p className = "Woman-p"> ''I needed some fresh picked flowers as a gift for my
-        grandmother's birthday, and a friend referenced flower
-        shop to me. Suffice to say I don't have any regrets!''</p>
-                        <p className = "Woman-title">
+                    <img src = {Man} width = "400" height = "400" className = "man-img" />
+                    <img src = {Woman} width = "400" height = "400" className = "woman-img" />
+                    <img src = {Miles} width = "400" height = "400" className = "miles-img" />
+
+                    <div className = "man-text-container">
+                        <p className = "man-p"> ''I was in dire need of a quick way to get my flowers
+                            on time for my wedding day, I would like to thank Flower
+                            Shop for their efforts on showing up in time.''</p>
+                        <p className = "man-title">
+                            -Melvin Jones
+                        </p>
+                    </div>
+                    <div className = "woman-text-container">
+                        <p className = "woman-p"> ''I needed some fresh picked flowers as a gift for my
+                        grandmother's birthday, and a friend referenced flowershop to me. Suffice to say
+                            I don't have any regrets!''
+                        </p>
+                        <p className = "woman-title">
                         -Vanessa Richardson 
                         </p>
                     </div>
-                </div>
-
-                <div className = "Miles-testimony-div">
-                    <img src = {Miles} width = "400" height = "400" className = "Miles-img" />
-                    <div>
-                        <p className = "Miles-p"> ''An old friend of mine was sick and I was looking to
-        purchase some flowers as a gift and send it to the
-        hospital where he was resting. I thank Flower Shop
-        for their beautifully picked flowers.''</p>
-                        <p className = "Miles-title">
+                    <div className = "miles-text-container">
+                        <p className = "miles-p"> ''An old friend of mine was sick and I was looking to
+                        purchase some flowers as a gift and send it to the hospital where he was resting.
+                        I thank Flower Shop for their beautifully picked flowers.''
+                        </p>
+                        <p className = "miles-title">
                         -Miles Edgeworth
                         </p>
                     </div>
                 </div>
             </div>
         </div>
-
-
     )
 };
 
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import HomeWrapper from '../Home/HomeWrapper.js'
-import AboutTest from '../About/AboutTest.js'
+import About from '../About/About.js'
 import ShopWrapper from "../Shop/ShopWrapper"
 
 const Main = () => {
@@ -9,7 +9,7 @@ return (
    <main>
     <Switch>
       <Route path='/home'> <HomeWrapper/> </Route>
-      <Route path='/about'> <AboutTest/> </Route>
+      <Route path='/about'> <About/> </Route>
       <Route path ="/shop"> <ShopWrapper/> </Route>
     </Switch>
   </main>
